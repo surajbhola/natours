9514952d3a00900ce7ffc0eb017be16e05e4d52e
Suraj Bhola <surajbhola1001@gmail.com>
2024-01-17 21:04:09 +0530

    package.json updated

--- app.js
+++ app.js
@@ -43,12 +43,12 @@ if (process.env.NODE_ENV === "development") {
 
 
 // decides how many requests per IP will allowed in several times
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many request from this IP, please try again in an hour",
-});
-app.use("/api", limiter);
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: "Too many request from this IP, please try again in an hour",
+// });
+// app.use("/api", limiter);
 
 // bodyparser , reading data from body into req.body
 app.use(express.json({ limit: "10kb" })); // middleware fzunction to get data using req object in post method
